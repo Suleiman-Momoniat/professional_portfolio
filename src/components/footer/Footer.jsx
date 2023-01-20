7dadd274f890e6a70e84388b722efa89a9083bc0
Suleiman-Momoniat <suleiman.momoniat@gmail.com>
2023-01-20 08:27:03 +0200

    feat(fronend):added ai image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Suli</a>
+      <a href="#" className="footer__logo">
+        Suli
+      </a>
 
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
         {/* <li><a href="#services">Services</a></li> */}
         {/* <li><a href="#portfolio">Portfolio</a></li> */}
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footer__copyright">
         <small>&copy; Suli. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
